e12f45171f64251a22d9ef9278b34ca257821907
mhmo91 <eng.mhmo91@gmail.com>
2023-12-09 10:38:54 +0100

    fixes

--- src/types/scss.d.ts
+++ src/types/scss.d.ts
@@ -8,3 +8,14 @@ declare module '*.module.scss' {
 	const classes: Record<string, string>
 	export default classes
 }
+
+declare module '*.css?inline' {
+	import { type CSSResult } from 'lit'
+	const styles: CSSResult
+	export default styles
+}
+
+declare module '*.module.css' {
+	const classes: Record<string, string>
+	export default classes
+}
